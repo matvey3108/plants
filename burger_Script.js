36042fd5479657ed0ebb1e70d9c098a18ccb94ce
matvey3108 <andrikovm@gmail.com>
2025-10-16 18:20:14 +0300

    реализовал бургер меню

--- burger_Script.js
+++ burger_Script.js
@@ -2,15 +2,7 @@ const burgerMenu = document.querySelector('.burger-menu')
 const burgerNav = document.querySelector('.menu-navigation')
 
 burgerMenu.addEventListener('click', burgerButton)
-let isHave = false
 
 function burgerButton() {
-    if(!isHave) {
-        burgerNav.style.left = '0px'
-        isHave = true
-    }  
-    else {
-        burgerNav.style.left = '-190px'
-        isHave = false
-    }
+    burgerNav.classList.toggle('menu-navigation--active')
 }
